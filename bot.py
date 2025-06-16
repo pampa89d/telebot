1e95ebe690ec4d0ec34f90bf3e8532855e444adf
pampa89d <pampa89d@gmail.com>
2025-06-16 23:39:07 +0300

    Скорректировано задание, добавлен транслит ФИО. Осталась ошибка - docker не выводит лог в терминалена запущенном контейнере

--- bot.py
+++ bot.py
@@ -27,11 +27,33 @@ async def proccess_command_start(message: Message):
     await bot.send_message(chat_id=user_id, text=text)
 
 # 4.proccessing all messages
+
+def translite(mes):
+        translite_name = []
+        not_mapping = []
+        slovar = {'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'yo',
+            'ж':'zh','з':'z','и':'i','й':'i','к':'k','л':'l','м':'m','н':'n',
+            'о':'o','п':'p','р':'r','с':'s','т':'t','у':'u','ф':'f','х':'kh',
+            'ц':'c','ч':'ch','ш':'sh','щ':'sch','ъ':'','ы':'y','ь':'','э':'e',
+            'ю':'u','я':'ya','А':'A','Б':'B','В':'V','Г':'G','Д':'D','Е':'E','Ё':'YO',
+            'Ж':'ZH','З':'Z','И':'I','Й':'I','К':'K','Л':'L','М':'M','Н':'N',
+            'О':'O','П':'P','Р':'R','С':'S','Т':'T','У':'U','Ф':'F','Х':'KH',
+            'Ц':'C','Ч':'CH','Ш':'SH','Щ':'SCH','Ъ':'','Ы':'y','Ь':'','Э':'E',
+            'Ю':'U','Я':'YA', ' ': ' '}
+        for char in mes:
+            if char in slovar.keys():
+                translite_name.append(slovar[char])
+            else:
+                not_mapping.append(char)
+        if len(not_mapping) > 0:
+            print('Not mapping char: %s' % (not_mapping))
+        return 'Your name in transliteration: ' + ''.join(translite_name)
+
 @dp.message()
 async def send_echo(message: Message):
     user_name = message.from_user.full_name
     user_id = message.from_user.id
-    text = message.text
+    text =  translite(message.text)
     logging.info(f'{user_name} {user_id}: {text}')
     await message.answer(text=text)
 
